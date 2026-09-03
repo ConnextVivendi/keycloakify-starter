914f301e6adafb5b31e00937aa8f34b39cb88311
Daniel Brüggemann <daniel.brueggemann@connext.de>
2026-09-03 11:07:51 +0200

    feat: enhance EmailConfirmation component with dynamic class handling for buttons

--- src/login/pages/EmailConfirmation.tsx
+++ src/login/pages/EmailConfirmation.tsx
@@ -1,9 +1,12 @@
+import { getKcClsx } from "keycloakify/login/lib/kcClsx";
 import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
 
 export default function EmailConfirmation(props: PageProps<Extract<KcContext, { pageId: "email-confirmation.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
+
+    const { kcClsx } = getKcClsx({ doUseDefaultCss, classes });
 
     const { msg } = i18n;
 
@@ -23,9 +26,15 @@ export default function EmailConfirmation(props: PageProps<Extract<KcContext, { 
             <div>
                 <div className="kc-email-confirmation-message">{msg("magicLinkSuccessfulLogin")}</div>
                 {magicLinkContinuation.sameBrowser && (
-                    <a href={magicLinkContinuation.url} id="mode-barcode">
-                        {msg("loginPage")}
-                    </a>
+                    <p>
+                        <a
+                            href={magicLinkContinuation.url}
+                            id="mode-barcode"
+                            className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonBlockClass", "kcButtonLargeClass")}
+                        >
+                            {msg("loginPage")}
+                        </a>
+                    </p>
                 )}
             </div>
         </Template>
